e36581ee24d73df69a7b6560042c0ad0eb655714
Pavol Rajzak <pavol.rajzak@itera.no>
2016-11-15 17:11:28 +0100

    Add simple reading from websocket when timer is started

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import reducer from "./reducers";
 import startTimerSaga from "./sagas";
 import {MainContainer} from "./containers";
 
+import SockJs from "sockjs-client";
+import Stomp from "stompjs";
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -15,6 +18,19 @@ const store = createStore(
 );
 
 sagaMiddleware.run(startTimerSaga);
+
+function connect() {
+    const socket = new SockJs("http://localhost:9090/websocket");
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({}, (frame) => {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/timers', (message) => {
+            console.log(message.body);
+        });
+    });
+}
+
+connect();
 
 render(
     <Provider store={store}>
